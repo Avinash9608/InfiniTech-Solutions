d592dc129d55d177824b316a61fef324275d4fd4
Avinash KUMAR <Avinashmadhukar4@gmail.com>
2025-06-29 15:02:56 +0000

    add  this image https://images.unsplash.com/photo-1667984390527-850f631

--- app/services/cloud-services/page.tsx
+++ app/services/cloud-services/page.tsx
@@ -198,11 +198,11 @@ export default function CloudServicesPage() {
               transition={{ type: 'spring', stiffness: 300 }}
             >
               <Image
-                src="https://images.unsplash.com/photo-1607798734342-3e2b6408c8e2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://images.unsplash.com/photo-1667984390527-850f63192709?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Cloud infrastructure"
                 fill
                 className="object-cover"
-                data-ai-hint="cloud infrastructure"
+                data-ai-hint="abstract network"
               />
             </motion.div>
           <div>
