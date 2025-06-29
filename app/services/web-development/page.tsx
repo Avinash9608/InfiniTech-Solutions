4a11af8488209e822ab66a95ac316b0acc1ee357
Avinash KUMAR <Avinashmadhukar4@gmail.com>
2025-06-29 14:26:50 +0000

    add this image https://plus.unsplash.com/premium_photo-1681488262364-8ae

--- app/services/web-development/page.tsx
+++ app/services/web-development/page.tsx
@@ -82,8 +82,8 @@ const subServices = [
 ];
 
 const caseStudies = [
-  { id: '1', title: 'E-commerce Platform', description: 'Fully integrated online store with payment gateway & admin dashboard', imageUrl: 'https://placehold.co/600x400.png', dataAiHint: 'ecommerce dashboard' },
-  { id: '5', title: 'Restaurant Booking System', description: 'Responsive web app for real-time table reservations', imageUrl: 'https://placehold.co/600x400.png', dataAiHint: 'restaurant app' },
+  { id: '1', title: 'E-commerce Platform', description: 'Fully integrated online store with payment gateway & admin dashboard', imageUrl: 'https://plus.unsplash.com/premium_photo-1681488262364-8aeb1b6aac56?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', dataAiHint: 'ecommerce dashboard' },
+  { id: '5', title: 'Restaurant Booking System', description: 'Responsive web app for real-time table reservations', imageUrl: 'https://images.unsplash.com/photo-1724426057815-a12b34f027c1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHJlc3R1cmFudHxlbnwwfHwwfHx8MA%3D%3D', dataAiHint: 'restaurant app' },
 ];
 
 export default function WebDevelopmentPage() {
